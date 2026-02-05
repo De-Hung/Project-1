3985eb51f0e78069f2124e6553b94a638e8216c8
Derian H <derianhung@tamu.edu>
2026-02-04 22:54:13 -0600

    project 1 files

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    const savedTheme = localStorage.getItem('theme');
+    const themeElement = document.getElementById('theme-sheet');
+    
+    // If a theme was previously saved, apply it
+    if (savedTheme) {
+        themeElement.setAttribute('href', savedTheme);
+    }
+};
+
+function toggleStyle() {
+    let themeElement = document.getElementById('theme-sheet');
+    let currentTheme = themeElement.getAttribute('href');
+    let newTheme = '';
+
+    // Swap the theme and save the new selection to localStorage
+    if (currentTheme === 'style1.css') {
+        newTheme = 'style2.css';
+    } else {
+        newTheme = 'style1.css';
+    }
+
+    themeElement.setAttribute('href', newTheme);
+    localStorage.setItem('theme', newTheme); // This "remembers" the choice
+}
